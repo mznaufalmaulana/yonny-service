173c24f42232a45e555ebb309081b37013ae8a2c
gusnaikhsan <gusna.ikhsan7@gmail.com>
2021-12-17 10:56:38 +0700

    update

--- database/migrations/2021_11_11_074757_create_region_table.php
+++ database/migrations/2021_11_11_074757_create_region_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRegionTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('ms_region', function (Blueprint $table) {
@@ -20,11 +15,6 @@ class CreateRegionTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('ms_region');
