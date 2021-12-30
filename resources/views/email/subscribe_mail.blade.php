55b8cc8013a4ad0d9b29e06ca04a482ee34c2a53
gusnaikhsan <gusna.ikhsan7@gmail.com>
2021-12-31 00:48:53 +0700

    fixing email

--- resources/views/email/subscribe_mail.blade.php
+++ resources/views/email/subscribe_mail.blade.php
@@ -7,7 +7,7 @@
         <p class="title-email"><strong>Hello there,</strong><br> Thank you for your subscription. News product update will be ready for you.</p>
       </div>
       <div class="content-img">
-        <img class="img-email" src="{{asset('image/contohBY.png')}}}}" alt="promo">
+        <img class="img-email" src="{{asset('image/contohBY.png')}}" alt="promo">
       </div>
       <div style="margin-top: 20px">
         Thank you, <br> <strong>BATUYONNY</strong>
