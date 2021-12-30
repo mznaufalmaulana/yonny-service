5fb21ec3b121c90c0f8b8b8566f044b7ebeb1521
gusnaikhsan <gusna.ikhsan7@gmail.com>
2021-12-31 01:32:32 +0700

    fixing email link

--- resources/views/email/broadcast_mail_promo.blade.php
+++ resources/views/email/broadcast_mail_promo.blade.php
@@ -7,7 +7,7 @@
           <p class="title-email"><strong>Hello there,</strong><br> New collection special for you . . . !!!</p>
         </div>
         <div class="content-img">
-          <img class="img-email" src="{{asset("storage/".$data['photo'])}}" alt="promo">
+          <img class="img-email" src="{{asset("storage").'/'.$data['photo']}}" alt="promo">
         </div>
         <div class="content-button">
           <a href="{{$data['link']}}">
