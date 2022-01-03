c4f4233959ea531bced55957ffc29cabcbe1a973
gusnaikhsan <gusna.ikhsan7@gmail.com>
2022-01-03 15:23:14 +0700

    fixing

---
+++ tests/Feature/Http/Controllers/Master/RegionControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Master;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegionControllerTest extends TestCase
+{
+    public function testRegionWithNotAuth()
+    {
+      $response = $this->get('/admin/region/list');
+      $response->assertStatus(302);
+    }
+}
