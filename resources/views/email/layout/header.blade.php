41216bf29de9a859d9805040ec0b66e980fcd9d4
gusnaikhsan <gusna.ikhsan7@gmail.com>
2022-04-22 23:35:25 +0700

    -fixing email

--- resources/views/email/layout/header.blade.php
+++ resources/views/email/layout/header.blade.php
@@ -1,6 +1,6 @@
 @section('header')
   <div class="logo-by">
-    <a href="#">
+    <a href="https://batuyonny.com">
         <img src={{asset("email-logo.png")}} alt="batuyonny" data-auto-embed="attachment">
     </a>
   </div>
